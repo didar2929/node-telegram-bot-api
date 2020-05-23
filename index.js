bfe3883a2d45b40f3bdd8be758081b8deff99de0
didar2929 <59870374+didar2929@users.noreply.github.com>
2020-05-23 17:42:13 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,13 @@ if (majorVersion <= 5) {
 } else {
   module.exports = require('./src/telegram');
 }
+
+const TelegramBot = require('node-telegram-bot-api')
+
+const TOKEN = '1219108831:AAH2UkJk4QOgiwCBiF-9r3WuqfoZn43A8VA'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from bot Chompy. Chompy says: "Hi, ${msg.from.first_name}"`)
+})
